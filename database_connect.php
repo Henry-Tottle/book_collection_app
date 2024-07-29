d5a6ed7199166cb7991bf9251bac150895980b5d
Henry Tottle <henrywilliamtottle@gmail.com>
2024-07-29 15:53:34 +0100

    Connected database

--- database_connect.php
+++ database_connect.php
@@ -1,10 +1,10 @@
 <?php
-$db = new PDO('mysql:host=db; dbname=','root','password');
+$db = new PDO('mysql:host=db; dbname=lit_logger','root','password');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 
 $query = $db->prepare('SELECT * FROM `books`');
-$query = execute();
+$query->execute();
 $results = $query->fetchAll();
 var_dump($results);
